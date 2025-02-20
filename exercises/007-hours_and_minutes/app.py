6cc013a0e7f00e6c37200875cafbc5b0aab046c6
Sergio Roque Hernández <sergioroquehernandez93@gmail.com>
2025-02-20 17:49:57 +0000

    0225

--- exercises/007-hours_and_minutes/app.py
+++ exercises/007-hours_and_minutes/app.py
@@ -1,6 +1,12 @@
 def hours_minutes(seconds):
   # Your code here
-  return None
+  horas = seconds//3600
+  minutos = (seconds%3600)//60
+  segundos = (seconds%3600)%60
+
+
+  return horas, minutos, segundos
 
 # Invoke the function and pass any integer as its argument
-print(hours_minutes(3900))
+print(hours_minutes(3905))
+
