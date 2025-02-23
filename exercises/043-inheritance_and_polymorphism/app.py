5662e64d63ac2cbf132585f3baa2031448850e05
Sergio Roque Hernández <sergioroquehernandez93@gmail.com>
2025-02-23 17:16:57 +0000

    Hasta_el_43

--- exercises/043-inheritance_and_polymorphism/app.py
+++ exercises/043-inheritance_and_polymorphism/app.py
@@ -15,3 +15,18 @@ class Student:
 ### DON'T modify the code above ###
         
 ### ↓ Your code here ↓ ###
+class CollegeStudent(Student):
+    def __init__(self, name, age, grade, major):
+        super().__init__(name, age, grade)
+        self.major = major
+    
+    def introduce(self):
+        return f"Hi there! I'm {self.name}, a college student majoring in {self.major}."
+
+    def attend_lecture(self):
+        return f"{self.name} is attending a lecture for {self.major} students"
+
+
+student1 = CollegeStudent("Sergio", 31, 94, "Electrician")
+print(student1.introduce())
+print(student1.attend_lecture())
